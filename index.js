ac2ae0f0c9fb6b783d7b4884e8228efa3da4804b
SCHOOLAAD <71494177+SCHOOLAAD@users.noreply.github.com>
2021-01-09 10:31:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ let on;
 let app = express();
 let server = app.listen(process.env.PORT, console.log(`listening to port `+process.env.PORT));
 
-client.once('ready', () => {
-	console.log('Ready!');
-});
 
 function init(){
 	client = new Discord.Client();
@@ -28,6 +25,10 @@ function init(){
 		// other commands...
 	});
 	on = true;
+	client.once('ready', () => {
+		console.log('Ready!');
+	});
+
 }
 init();
 function boek(args, msg){
@@ -35,7 +36,7 @@ function boek(args, msg){
 	else if(args[0] == "scheikunde" || args[0] == "sc" || args[0] == "sk") msg.channel.send('http://v3dbot.000webhostapp.com/v3d?url=skvlb');
 	else if(args[0] == "natuurkunde" || args[0] == 'na' || args[0] == 'ns' || args[0] == 'nk') msg.channel.send('http://v3dbot.000webhostapp.com/v3d?url=navg');
 	else if(args[0] == "engels" || args[0] == 'en' || args[0] == 'eng') msg.channel.send('http://v3dbot.000webhostapp.com/v3d?url=envg');
-	else msg.channel.send('Ik weet niet wat je bedoelt?\nIk ken Engels (en, eng)\nIk ken natuurkunde (na, nk, ns)\nIk ken scheikunde (sk, sc)');
+	else msg.channel.send('Ik weet niet wat je bedoelt?\nIk ken Engels (en, eng)\nIk ken natuurkunde (na, nk, ns)\nIk ken scheikunde (sk, sc)\n En ik ken Frans (fr, fa)');
 }
 
 function ant(args, msg){
