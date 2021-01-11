5141fc68560e66cdc39bd11f09ef58da9ec1d8f6
SCHOOLAAD <71494177+SCHOOLAAD@users.noreply.github.com>
2021-01-11 12:49:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -62,6 +62,6 @@ function keep(req, res){
 function pl(args, msg){
 	if(args[0] == "thijs" || args[0] == 'thijsmuziek') msg.channel.send('https://open.spotify.com/playlist/7BF0UCW0fLmLV9sOB0PSLo');
 	else if(args[0] == "throwbacks" || args[0] == 'throwback' || args[0] == 'adriaan') msg.channel.send('https://open.spotify.com/playlist/6gYs81b1TVlI3BzQuspNv8');
-	else if(args[0] == "shit" || args[0] == 'slecht' || (args[0] == 'lif' && args[0] == 'laf' (args[0] == 'lof' || args[0] == 'lofjes'))) msg.channel.send('https://open.spotify.com/playlist/6gYs81b1TVlI3BzQuspNv8');
+	else if(args[0] == "shit" || args[0] == 'slecht' || (args[0] == 'lif' && args[0] == 'laf' && (args[0] == 'lof' || args[0] == 'lofjes'))) msg.channel.send('https://open.spotify.com/playlist/6gYs81b1TVlI3BzQuspNv8');
 	else msg.channel.send('Ik weet niet wat je bedoelt?\nIk ken thijsmuziek (thijs)\nIk ken throwbacks (adriaan)\nEn ik ken slechte muziek (shit, slecht, lif laf lof(jes))');
 }
